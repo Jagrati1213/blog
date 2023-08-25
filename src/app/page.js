c572875cdf515721fd858267e7b1a4535050806e
Jagrati Gupta <123jagratigupta@gmail.com>
2023-08-25 14:05:54 +0530

    JG: client side rendering

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
+'use client' //client side rendering
 import styles from './page.module.css'
-
 export default function Home() {
+
+  // function with parameter
+  const apple = (name) => {
+    alert("fruit " + name);
+  }
+
   return (
     <main className={styles.main}>
-      <User name="Jagrati" />
+      {/* <User name="Jagrati" /> */}
+      <h1>Event, function & state</h1>
+      {/* if we directly called function it used server side rendering */}
+      {/* <button onClick={() => apple('jagrati')}>click</button> */}
     </main>
   )
 }
 
+// component
 const User = ({ name = 'xyz' }) => {
   return (
     <p>user name is {name}</p>
