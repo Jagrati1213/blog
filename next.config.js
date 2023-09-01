465f08bb82ea4504829f7df4b95bc66dc2db785f
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-01 16:32:36 +0530

    JG: learn redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    // redirects: async () => {
+    //     return [
+    //         {
+    //             source: '/about/:id',
+    //             destination: '/',
+    //             permanent: false,
+    //         }
+    //     ]
+    // }
+}
 
 module.exports = nextConfig
