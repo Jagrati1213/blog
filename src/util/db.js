71775fb646b76c93ca31eec6c9339332e5f666dd
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-01 16:37:34 +0530

    jg: db

---
+++ src/util/db.js
@@ -0,0 +1,27 @@
+export const UserDetails = [
+    {
+        id: 1,
+        name: 'maggu',
+        email: 'maggu1213@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'jaggu',
+        email: 'jaggu1213@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'siddhu',
+        email: 'siddhu1213@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'siddharth',
+        email: 'siddharth1213@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'sid',
+        email: 'sid1213@gmail.com'
+    },
+]
