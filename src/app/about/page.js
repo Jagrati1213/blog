36f47530d945908b412eff52cd362ba8c1e85c63
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-03 11:18:34 +0530

    jg: change api, set data, update & delete button functionality

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React from 'react'
 
 const getUserDetails = async () => {
-    let res = await fetch('http://localhost:3000/api/user');
+    let res = await fetch('http://localhost:3000/api/products');
     let data = await res.json();
     if (data.success) {
         return data.result;
@@ -16,18 +16,32 @@ const About = async () => {
     return (
         <div>
             <h2>userDetails page</h2>
-            <ol>
-                {
-                    result.map(item => {
-                        return <li key={item.id}>
-                            <p>{item.name} </p>
-                            <p>{item.email}</p>
-                            <Link href={`about/${item.id}/updateuser`}> Edit</Link>
-                            <Delete id={item.id} />
-                        </li>
-                    })
-                }
-            </ol>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Company</td>
+                        <td>Price</td>
+                        <td>Colors</td>
+                        <td>Category</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        result.map(item => {
+                            return <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.company}</td>
+                                <td>{item.price}</td>
+                                <td>{item.color}</td>
+                                <td>{item.category}</td>
+                                <td><Link href={`about/${item._id}`}>Update</Link></td>
+                                <td> <Delete id={item._id} /></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
 
             <Link href={'/adduser'}> Add new user</Link>
         </div>
