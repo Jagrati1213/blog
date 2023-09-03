2f52ebfc160d5a01731624da2420a584961e9d7b
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-03 11:20:22 +0530

    jg: change api

--- src/component/Delete.js
+++ src/component/Delete.js
@@ -3,14 +3,14 @@ import React from 'react'
 
 function Delete({ id }) {
     const handleDelete = async () => {
-        let res = await fetch(`http://localhost:3000/api/user/${id}`, {
+        let res = await fetch(`http://localhost:3000/api/products/${id}`, {
             method: 'DELETE'
         });
         res = await res.json();
         if (res.success) {
             alert('deleted')
         } else {
-            alert("there is some issue...")
+            alert("there is some issue...");
         }
     }
     return (
