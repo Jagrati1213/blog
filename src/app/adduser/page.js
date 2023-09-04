80f90c8cb2fb179c87a24a0f9b3958959f00e5e6
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-04 16:58:50 +0530

    jg; add form component

--- src/app/adduser/page.js
+++ src/app/adduser/page.js
@@ -1,51 +1,10 @@
-"use client";
-import Link from 'next/link';
-import React, { useState } from 'react'
+import Adduserform from '@/component/Adduserform';
+import { Host } from '@/feature/main';
 
-function AddUser() {
-
-    const [data, setData] = useState({ name: '', category: '', company: '', price: '', color: [] }); //store data
-
-    const handleChange = (event) => {
-        let key = event.target.name;
-        let value = event.target.value;
-        setData({ ...data, [key]: value });
-    }
-
-    // post the data in database...
-    const addUser = async () => {
-        let res = await fetch(`http://${hostName}/api/products`, {
-            method: 'POST',
-            body: JSON.stringify(data)
-        });
-        res = await res.json();
-        if (res.success) {
-            alert('updated');
-        }
-        else {
-            alert(res.result);
-        }
-    }
-
+async function AddUser() {
+    const hostName = await Host();
     return (
-        <div>
-            <h2>AddUser</h2>
-            <div>
-                <input type="text" placeholder='Enter name' name='name' value={data.name} onChange={(e) => handleChange(e)} />
-
-                <input type="text" placeholder='Enter category' name='category' value={data.category} onChange={(e) => handleChange(e)} />
-
-                <input type="text" placeholder='Enter company' name='company' value={data.company} onChange={(e) => handleChange(e)} />
-
-                <input type="text" placeholder='Enter price' name='price' value={data.price} onChange={(e) => handleChange(e)} />
-
-                <input type="text" placeholder='Enter color' name='color' value={data.color} onChange={(e) => handleChange(e)} />
-
-
-                <button onClick={addUser}>Add user</button>
-            </div>
-            <Link href={'/about'}> Back to about</Link>
-        </div>
+        <Adduserform hostName={hostName} />
     )
 }
 
