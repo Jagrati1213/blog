c46fee104ebe3e875c9e56d5de90cfbf2c24184c
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-04 13:37:58 +0530

    jg:change base url

--- src/utils/dbconnection.js
+++ src/utils/dbconnection.js
@@ -1,3 +1,3 @@
-const { MONGODB_URI } = process.env;
+const { MONGODB_URI, BASE_URL } = process.env;
 export const connectionSrt = MONGODB_URI;
-export let baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://blog.vercel.app';
+export let baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : BASE_URL;
