dfa338366bfab354a16a0ffa316f2194f7f602a6
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-04 11:13:32 +0530

    jg: add env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    env: {
+        // Define your environment variables here
+        My_USERNAME: process.env.My_USERNAME,
+        PASSWORD: process.env.PASSWORD,
+    },
+}
 
 module.exports = nextConfig
