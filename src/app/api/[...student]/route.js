04a294225b3c991f8fb5420e7a0b020bd89f9045
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-04 11:53:32 +0530

    jg: delete

--- src/app/api/[...student]/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-// dynamic route
-export function GET(_, content) {
-    const studentDetails = content.params.student;
-    return NextResponse.json({ result: studentDetails }, { status: 200 });
-}
