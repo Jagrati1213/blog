7b8999a2c994fc36335f5af5bae3184d00f06945
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-05 10:13:07 +0530

    jg: delete

--- src/feature/main.js
+++
@@ -1,9 +0,0 @@
-import { headers } from "next/headers";
-
-
-export function Host() {
-    const headersList = headers();
-    const hostName = headersList.get('host');
-    return hostName;
-}
-// to get domain
